186a35e54eec5c811e2c54b93f2f70fb75e2047f
KidEinstein <anirudhshekh@gmail.com>
2015-06-24 00:28:56 +0530

    added 404, switched off debug

--- results/settings.py
+++ results/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sdid)kw2a*3b!b-7qj(i))bi0@1f3s52wyidwff)d!fk=5)b7w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
